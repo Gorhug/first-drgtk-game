f1c8d8db436a3b43f61680a09b65bb8b7347572a
Ilkka Forsblom <quiverth@gmail.com>
2024-05-22 00:26:45 +0300

    analogitikku toimii

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,85 +1,56 @@
 def tick args
-  args.state.logo_rect ||= { x: 576,
+  args.state.player_rect ||= { x: 576,
                              y: 200,
-                             w: 128,
-                             h: 101 }
+                             w: 100,
+                             h: 80 }
+  speed = 25
+  dead_zone = 0.10
+  # args.outputs.labels  << { x: 640,
+  #                           y: 600,
+  #                           text: 'Hello World!',
+  #                           size_px: 30,
+  #                           anchor_x: 0.5,
+  #                           anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs/docs.txt directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "If you prefer formatted docs, you can access them locally at http://localhost:9001 or online at http://docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  args.outputs.sprites << { x: args.state.player_rect.x,
+                            y: args.state.player_rect.y,
+                            w: args.state.player_rect.w,
+                            h: args.state.player_rect.h,
+                            path: 'sprites/misc/dragon-0.png',
+                            # angle: args.state.tick_count
+                          }
 
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
+  if args.inputs.controller_one.left_analog_active? threshold_perc: dead_zone
+    args.state.player_rect.x += args.inputs.controller_one.left_analog_x_perc * speed
+    args.state.player_rect.y += args.inputs.controller_one.left_analog_y_perc * speed
+  else
+    if args.inputs.left
+      args.state.player_rect.x -= speed
+    elsif args.inputs.right
+      args.state.player_rect.x += speed
+    end
 
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
-
-  if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
-  elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+    if args.inputs.down
+      args.state.player_rect.y -= speed
+    elsif args.inputs.up
+      args.state.player_rect.y += speed
+    end
   end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+  # wraparound
+  if args.state.player_rect.x > 1280
+    args.state.player_rect.x = 0
+  elsif args.state.player_rect.x < 0
+    args.state.player_rect.x = 1280
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
-  end
-
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
+  if args.state.player_rect.y > 720
+    args.state.player_rect.y = 0
+  elsif args.state.player_rect.y < 0
+    args.state.player_rect.y = 720
   end
 end
